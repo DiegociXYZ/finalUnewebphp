ddfd5de3e19fc211034cb05e7dd7f25b698f422b
DiegociXYZ <diegoci268@gmail.com>
2024-12-21 10:48:12 -0400

    Before middleware

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,12 @@ use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 //user controller routes
-Route::get('/', [UserController::class,"showCorrectHomepage"]);
+Route::get('/', [UserController::class,"showCorrectHomepage"])->name('login');
 Route::post('/register', [UserController::class, "register"]);
 Route::post('/login', [UserController::class, "login"]);
 Route::post('/logout', [UserController::class, "logout"]);
 
 //blogpost
-Route::get('/create-post', [PostController::class, 'showCreateForm']);
+Route::get('/create-post', [PostController::class, 'showCreateForm'])->middleware('guest');
 Route::post('/create-post', [PostController::class, 'storeNewPost']);
 Route::get('/post/{post}', [PostController::class, 'viewSinglePost']);
