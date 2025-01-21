767ad325bcadec830780ca7a0d927c81426ffb94
DiegociXYZ <diegoci268@gmail.com>
2025-01-21 19:41:25 -0400

    FEAT: Lista de usuarios

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -45,7 +45,7 @@ class UserController extends Controller
         if(auth()->check()){
             $currentlyFollowing = Follow::where([['user_id', '=', auth()->user()->id],['followeduser', '=', $user->id]])->count();
         }
-        View::share('sharedData', ['currentlyFollowing'=>$currentlyFollowing, 'avatar' =>$user->avatar, 'username' => $user->username, 'postCount' => $user->posts()->count()]);
+        View::share('sharedData', ['currentlyFollowing'=>$currentlyFollowing, 'avatar' =>$user->avatar, 'username' => $user->username, 'postCount' => $user->posts()->count(), 'followerCount' => $user->followers()->count(), 'followingCount' =>$user->followingTheseUsers()->count()]);
     }
     public function profile(User $user) {
         $this->getSharedData($user);
@@ -53,12 +53,12 @@ class UserController extends Controller
     }
     public function profileFollowers(User $user) {
         $this->getSharedData($user);
-        return view('profile-followers', ['posts' => $user->posts()->latest()->get()]);
+        return view('profile-followers', ['followers' => $user->followers()->latest()->get()]);
 
     }
     public function profileFollowing(User $user) {
         $this->getSharedData($user);
-        return view('profile-following', ['posts' => $user->posts()->latest()->get()]);
+        return view('profile-following', ['following' => $user->followingTheseUsers()->latest()->get()]);
 
     }
 
